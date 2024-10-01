54794f15e8f89577894a49ca2a15a95dc88feefa
T14-1182 <lyubimova.t@bk.ru>
2024-10-01 15:07:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+
+# Задача "Функциональное разнообразие":
+from random import choice
+
+"""
+Даны 2 строки:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+Необходимо составить lambda-функцию для следующего выражения - list(map(?, first, second)).
+Здесь ? - место написания lambda-функции.
+"""
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+total = list(map(lambda x, y: x == 'а', first, second))
+print(total)
+
+"""Замыкание:
+Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
+Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - 
+параметр принимающий неограниченное количество данных любого типа.
+Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
+Функция get_advanced_writer возвращает функцию write_everything.
+
+Данный код:
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+"""
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for item in data_set:
+                file.write(str(item) + '\n')
+        return
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+"""Метод __call__:
+Создайте класс MysticBall, объекты которого обладают атрибутом words хранящий коллекцию строк.
+В этом классе также определите метод __call__ который будет случайным образом выбирать слово из words и возвращать его. Для случайного выбора 
+с одинаковой вероятностью для каждого данного в коллекции можете использовать функцию choice из модуля random.
+"""
+
+
+class MysticBall(object):
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
